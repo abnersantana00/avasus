af009829d8aa80e5edcffb8238abcadb7bc76cae
Abner <abnersant10@gmail.com>
2022-12-26 15:11:20 -0300

    template de login 1

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import *
 # Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        cpf = request.POST.get('cpf')
+        senha = request.POST.get('senha')
+        # verrificar se CPF e Senha informada é igual no BD
+        user = authenticate(request, username=cpf, password=senha)
+        if user is not None:
+            login(request, user)
+            return redirect('pag-inicial')
+        else:
+            messages.error(request, 'CPF ou Senha Incorreta!')
+    return render(request, "login.html")
