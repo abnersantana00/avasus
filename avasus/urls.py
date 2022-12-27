cae7c25bb25b40cbd71ce94daf0c1412169ed561
Abner <abnersant10@gmail.com>
2022-12-27 14:22:57 -0300

    front end login e cadastro ok

--- avasus/urls.py
+++ avasus/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from forum.views import login, cadastro
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', login, name='login'),
+     path('cadastro', cadastro, name='cadastro'),
 ]
